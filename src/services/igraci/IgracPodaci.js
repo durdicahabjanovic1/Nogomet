459009f62f0c492781006072d8d6adbc8e851547
ĐURĐICA <durdicahabjanovic1@gmail.com>
2026-09-23 20:56:15 +0200

    Priprema podataka

---
+++ src/services/igraci/IgracPodaci.js
@@ -0,0 +1,13 @@
+
+export const igraci = [
+    {
+        sifra: 1,
+        ime: 'Marin',
+        prezime:'Habjanović',
+        brojDresa: 8,
+        brojKopackih: 42,
+        pozicija: 'desno krilo',
+        datumRodenja: '2010-10-15T00:00:00'
+        brojRegistracije:
+    }
+]
